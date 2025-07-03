6fd0b56939422597719d07e0e19c99f8cd10f867
Alfonzo <thulanikgakole@gmail.com>
2025-07-03 22:46:53 +0200

    I have done the first half of the Hero section and animated some of the words to flip and make the page looks much more interesting

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./section/Navbar.jsx";
+import Hero from "./section/Hero.jsx";
 
 const App = () => {
     return (
         <div className="container mx-auto max-w-7xl">
             <Navbar />
+            <Hero />
         </div>
     )
 }
---
+++ src/section/Hero.jsx
@@ -0,0 +1,11 @@
+import HeroText from "../components/HeroText.jsx";
+
+const Hero = () => {
+    return (
+        <section className={"flex items-start justify-start md:items-start md:justify-start min-h-screen overflow-hidden"}>
+            <HeroText />
+        </section>
+    )
+}
+
+export default Hero
