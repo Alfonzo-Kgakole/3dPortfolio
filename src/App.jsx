3c30d4f5edc468f056683939575d87566ab42cc2
Alfonzo <thulanikgakole@gmail.com>
2025-07-03 21:34:15 +0200

    starting project with the navigation section

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./section/Navbar.jsx";
+
+const App = () => {
+    return (
+        <div className="container mx-auto max-w-7xl">
+            <Navbar />
+        </div>
+    )
+}
+
+export default App
